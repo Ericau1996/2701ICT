73fbbf18d4c9e4abee0c28d5296c6e36ec071677
hiulok.au@griffithuni.edu.au <hiulok.au@griffithuni.uni.edu.au>
2019-05-13 13:15:44 +1000

    display the upload image

--- src/app/store/store.page.ts
+++ src/app/store/store.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { Store }         from '../store';
 import { StoreService }  from '../store.service';
+import {Storage} from '@ionic/storage';
 
 @Component({
   selector: 'app-store',
@@ -16,16 +17,35 @@ export class StorePage implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private storeService: StoreService,
-    private location: Location
+    private location: Location,
+    private storage: Storage
+    
   ) { }
 
   ngOnInit() {
     this.getStore();
   }
-
+  
   getStore(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.storeService.getStore(id)
     .subscribe(store => this.store = store);
   }
+
+  imageFile:any;
+  imageSelected(files){
+    if (files.length>0){
+      alert("imageSelected: "+files[0].name);
+    }
+
+    let fileReader = new FileReader();
+
+    fileReader.readAsDataURL(files[0]);
+
+    fileReader.onload=e=>{
+      this.imageFile = fileReader.result;
+      this.storage.set('avatar', this.imageFile);
+    };
+  }
+
 }
